9ab3c068b618af13af61e5e434efb5ae45d623d4
Miracle <mbo_seyifash@yahoo.com>
2024-07-31 23:07:14 +0400

    "Added react-icons dependency and imported FcGoogle and FaGithub icons in Auth page"

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -5,6 +5,8 @@ import { useCallback, useState } from "react";
 import axios from 'axios'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation';
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
   const router = useRouter();
@@ -97,6 +99,16 @@ const Auth = () => {
           <button onClick={variant === 'login'? login : register } className="bg-red-600 py-3 text-white rounded-md w-full mt-6 lg:mt-10 hover:bg-red-700 transition">
             {variant === 'login' ? 'Login' : 'Sign up'}
           </button>
+          <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+            <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+              <FcGoogle size={30}/>
+            </div>
+            <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+              <FaGithub size={30}/>
+            </div>
+
+
+          </div>
           <p className="text-neutral-500 mt-6 lg:mt-12 text-center">
             {
               variant === "login"
